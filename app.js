4c5f94b519a1816bc2d0feb5476404ad6c634073
Kumarajeeva Elavarasan <kumarjeeva94@gmail.com>
2022-02-12 18:26:28 -0500

    Updated mongo connect

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://Kumarajeeva:Mongodb29%40@cluster0.wzhai.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Kumarajeeva:Mongodb29%40@cluster0.wzhai.mongodb.net/todolistDB", {useNewUrlParser: true,useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
